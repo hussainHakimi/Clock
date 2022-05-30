7d34ff254aae3f1f15cf4b2264a4b99d939bcfe6
hussainHakimi <hussainbayan614@gmail.com>
2022-05-30 20:43:41 +0430

    project done.

--- script.js
+++ script.js
@@ -11,15 +11,15 @@
       secondHand.style.transform = `rotate(${secondDegree}deg)`;
       
       const minutes = now.getMinutes();
-      const minutesDegrees = ((minutes / 60) * 360) + 90;
+      const minutesDegrees = ((minutes / 60) * 360) + ((second / 60) * 6) + 90;
       minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
 
       const hour = now.getHours();
-      const hourDegrees = (hour / 12) * 360;
+      const hourDegrees = ((hour / 12) * 360) + ((minutes/60)*30) + 90 ;
       hourHand.style.transform = `rotate(${hourDegrees}deg)`;
     }
-
-
+    
+    
     setInterval(setTime, 1000);
-  
+    
     console.log(Date());
